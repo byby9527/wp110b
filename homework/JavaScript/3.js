7d1b3fed2ec901c48c30ea0a6ddd80b6c4808d29
byby9527 <99935115+byby9527@users.noreply.github.com>
2022-06-11 12:24:25 +0800

    Update 3.js

--- homework/JavaScript/3.js
+++ homework/JavaScript/3.js
@@ -1,1 +1,28 @@
+function isprime(n)
+{
+  if (n === 1)
+  {
+  
+    return; 
+  }
+  else if (n === 2)
+  {
+    return true;
+  }
+  else
+  {
+    
+  for (let i = 2; i < n; i++)
+  {
+      if (n%i === 0)
+      {
+       return false;
+      }
+  }
+  return true;
+  }
+}
+console.log(isprime(17));
+console.log(isprime(21));
+
 
